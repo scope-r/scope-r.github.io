b2e9aaa85ad9972623a63d54b5aa2e50903766dc
patrique.legault <patrique.legault@gmail.com>
2019-09-26 08:41:23 -0400

    Adding captcha v2

--- assets/js/scope-r.js
+++ assets/js/scope-r.js
@@ -9,7 +9,6 @@ $("#valid_date").html(
 
 $(".scroll-on-click").click(function(object) {
     var location = $(object.currentTarget).data("scrollTarget");
-    debugger;
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#" + location).offset().top
     }, 2000);

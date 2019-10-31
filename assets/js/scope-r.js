26a7cfc303aeff9427fd0f8902dfd416197fb0a0
patrique.legault <patrique.legault@gmail.com>
2019-10-31 08:59:11 -0400

    Adding the id's and fixed the URL,current issue is submission saying invalid request yet body looks fine

--- assets/js/scope-r.js
+++ assets/js/scope-r.js
@@ -5,27 +5,35 @@ var copyright_text = $("#valid_date").html();
 
 // Replace the year with the current year
 $("#valid_date").html(
-    copyright_text.replace("$date", this_year));
+  copyright_text.replace("$date", this_year));
 
-$("#contactform").submit(function(event){
+$("#contactform").submit(function (event) {
   event.stopImmediatePropagation();
   event.preventDefault();
 
   // Make sure that the captcha button was clicked
   var response = grecaptcha.getResponse();
   if (response != "") {
+
+    var payload = {
+      "text": $("#name").val() + $("#email_address").val() + $("#message").val()
+    };
+
+    var result = JSON.stringify(payload);
+
+    debugger;
     $.ajax({
-        url: "https://hooks.slack.com/services/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",
-        method: "POST",
-        contentType: "application/json",
-        data: '{"text":"Hello, World!"}', //TODO
-        success: function(response){
-          alert("Thank you for your message");
-        },
-        error: function(response){
-          alert("Error");
-          console.log(response);
-        }
+      url: "https://hooks.slack.com/services/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",
+      method: "POST",
+      contentType: "application/json",
+      data: result,
+      success: function (response) {
+        alert("Thank you for your message");
+      },
+      error: function (response) {
+        alert("Error");
+        console.log(response);
+      }
     });
   }
   else {

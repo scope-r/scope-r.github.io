1beae11e08f53f4423f8539515b4f5bc60311395
Patrique Legault <patrique.legault@aftia.com>
2019-12-08 22:05:17 -0500

    Adding files

---
+++ assets/js/scope-r-all.js
@@ -0,0 +1,44 @@
+var date = new Date();
+var this_year = date.getFullYear();
+
+var copyright_text = $("#valid_date").html();
+
+var endpoint = "aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVENRS0VCQTczL0JQU1RZUzEzSy9BRkJoVkpmRkloWGh6N0JvYkdNU09odFA=";
+
+// Replace the year with the current year
+$("#valid_date").html(
+  copyright_text.replace("$date", this_year));
+
+$("#form_submit").click(function (event) {
+  event.stopImmediatePropagation();
+  event.preventDefault();
+
+  // Make sure that the captcha button was clicked
+  var response = grecaptcha.getResponse();
+  if (response != "") {
+
+    var payload = {
+      "text": "`Name` " + $("#name").val() +
+        " `Email` " + $("#email_address").val() +
+        " `Message` " + $("#message").val()
+    };
+
+    $.post({
+      url: atob(endpoint),
+      data: JSON.stringify(payload),
+      success: function (response) {
+        alert("Thank you for your message");
+        $("#name").val('');
+        $("#email_address").val('');
+        $("#message").val('');
+      },
+      error: function (response) {
+        alert("Error");
+        console.log(response);
+      }
+    });
+  }
+  else {
+    alert('Please verify that you are not a robot.');
+  }
+});
